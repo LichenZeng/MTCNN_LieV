92ac31a0ebb1f0f777d7b499472e5c34c884324b
tensorflow1 <tensorflow1@liev.top>
2018-09-18 20:42:30 +0800

    PM: Implement simple IOU

--- pytorch_mtcnn/gen_mtcnn_data.py
+++ pytorch_mtcnn/gen_mtcnn_data.py
@@ -4,6 +4,8 @@ import os
 import sys
 import time
 from matplotlib import pyplot as plt
+import random
+from mtcnn_utils import iou, nms
 
 img_path = "../img_celeba_4dbg"
 label_path = "../img_celeba_4dbg/list_bbox_celeba.txt"
@@ -16,7 +18,7 @@ for i, line in enumerate(open(label_path)):
     if i < 2:
         continue
     try:
-        print(line.split())
+        # print(line.split())
         line = line.split()
         img_name = line[0]
         x0 = int(line[1])
@@ -25,11 +27,32 @@ for i, line in enumerate(open(label_path)):
         height = int(line[4])
         x1 = x0 + width
         y1 = y0 + height
-        print(x0, y0, width, height, x1, y1)
+        # print(x0, y0, width, height, x1, y1)
         img = cv2.imread(os.path.join(img_path, img_name))
+        (h, w, c) = img.shape  # (h, w, c)
+
+        # print(h, w, c)
+
+        off_w = round(random.uniform(-0.8, 0.8) * width)
+        off_h = round(random.uniform(-0.8, 0.8) * height)
+        # print(off_w, off_h)
+        off_x0 = 0 if x0 + off_w <= 0 else x0 + off_w
+        off_x1 = w if x1 + off_w >= w else x1 + off_w
+
+        off_y0 = 0 if y0 + off_h <= 0 else y0 + off_h
+        off_y1 = h if y1 + off_h >= h else y1 + off_h
+
+        box = [x0, y0, x1, y1]
+        boxes = [off_x0, off_y0, off_x1, off_y1]
+        print("area: ", iou(boxes, box, True))
+        # img = img[y0:y1, x0:x1]
+        # save_name = "./crip{}.jpg".format(i)
+        # cv2.imwrite(save_name, img)
         cv2.rectangle(img, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2.rectangle(img, (off_x0, off_y0), (off_x1, off_y1), (255, 0, 0), 2)
+
         cv2.imshow("image", img)
-        cv2.waitKey(1000)
+        cv2.waitKey(5000)
 
         # plt.clf()
         # img = Image.open(os.path.join(img_path, img_name))  # type: Image.Image
---
+++ pytorch_mtcnn/mtcnn_utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random
+
+
+def iou(boxes, box, ismin=False):
+    # print("iou")
+    maxx0 = np.maximum(boxes[0], box[0])
+    maxy0 = np.maximum(boxes[1], box[1])
+    minx1 = np.minimum(boxes[2], box[2])
+    miny1 = np.minimum(boxes[3], box[3])
+    # print(maxx0, maxy0, minx1, miny1)
+    width = 0 if minx1 - maxx0 <= 0 else minx1 - maxx0
+    height = 0 if miny1 - maxy0 <= 0 else miny1 - maxy0
+    area_com = width * height
+    area_box = (box[2] - box[0]) * (box[3] - box[1])
+    area_boxes = (boxes[2] - boxes[0]) * (boxes[3] - boxes[1])
+
+    # print("area com: {}, boxes: {}, box: {}".format(area_com, area_boxes, area_box))
+
+    if ismin:
+        return area_com / np.minimum(area_boxes, area_box)
+    else:
+        return area_com / (area_boxes + area_box - area_com)
+
+
+def nms():
+    print("nms")
+
+
+if __name__ == '__main__':
+    width, height = 30, 35
+    box1 = [0, 0, 10, 10]
+    box2 = [0, 5, 10, 15]
+    box1 = np.array(box1)
+    box2 = np.array(box2)
+    area1 = iou(box2, box1, True)
+    area2 = iou(box2, box1)
+    print(area1, area2)
+    off_w = round(random.uniform(-1, 1) * (box1[2] - box1[0]))
+    off_h = round(random.uniform(-1, 1) * (box1[3] - box1[1]))
+    print(off_w, off_h)
+    off_x0 = 0 if box1[0] + off_w <= 0 else box1[0] + off_w
+    off_x1 = width if box1[2] + off_w >= width else box1[2] + off_w
+
+    off_y0 = 0 if box1[1] + off_h <= 0 else box1[1] + off_h
+    off_y1 = height if box1[3] + off_w >= height else box1[3] + off_h
+
+    print(off_x0, off_y0, off_x1, off_y1)
