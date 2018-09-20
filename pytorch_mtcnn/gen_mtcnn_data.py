f8f5ad99a347e3f59c94d852629c7f53e4544b3f
tensorflow1 <tensorflow1@liev.top>
2018-09-20 21:41:53 +0800

    Face: Finish data sample

--- pytorch_mtcnn/gen_mtcnn_data.py
+++ pytorch_mtcnn/gen_mtcnn_data.py
@@ -17,7 +17,7 @@ negative = "negative"
 
 COUNT = 50
 SCALE = 0.8
-SIZE = [48]
+SIZE = [48, 24, 12]
 
 # print(os.listdir(img_path))
 
@@ -28,17 +28,21 @@ for size in SIZE:
     pos_dir = os.path.join(size_save_path, positive)
     part_dir = os.path.join(size_save_path, part)
     neg_dir = os.path.join(size_save_path, negative)
-    pos_file = pos_dir + ".txt"
-    part_file = part_dir + ".txt"
-    neg_file = neg_dir + ".txt"
+    pos_name = pos_dir + ".txt"
+    part_name = part_dir + ".txt"
+    neg_name = neg_dir + ".txt"
 
     for dir in [pos_dir, part_dir, neg_dir]:
         if not os.path.exists(dir):
             os.makedirs(dir)
     dbg("dir", pos_dir, part_dir, neg_dir, lv=0)
-    dbg("file:", pos_file, part_file, neg_file, lv=0)
+    dbg("name:", pos_name, part_name, neg_name, lv=0)
 
     try:
+        pos_file = open(pos_name, "w")
+        part_file = open(part_name, "w")
+        neg_file = open(neg_name, "w")
+
         for i, line in enumerate(open(label_path)):
             if i < 2:
                 continue
@@ -82,68 +86,104 @@ for size in SIZE:
                     area = iou(boxes, box, False)
                     area_min = iou(boxes, box, True)
                     dbg(area, area_min, lv=0)
+
+                    off_x0 = float((x0 - neg_x0)) / neg_side
+                    off_y0 = float((y0 - neg_y0)) / neg_side
+                    off_x1 = float((x1 - neg_x1)) / neg_side
+                    off_y1 = float((y1 - neg_y1)) / neg_side
+
                     if area > 0.65 and count_pos < COUNT:
                         count_pos += 1
                         dbg("positive", lv=0)
                         crop_img = img[neg_y0:neg_y1, neg_x0:neg_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(pos_dir, i, count_pos)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 1 {} {} {} {}\n".format(save_name, off_x0, off_y0, off_x1, off_y1)
+                        pos_file.write(record)
+                        pos_file.flush()
 
                     elif area > 0.4 and count_part < COUNT:
                         count_part += 1
                         dbg("part", lv=0)
                         crop_img = img[neg_y0:neg_y1, neg_x0:neg_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(part_dir, i, count_part)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 2 {} {} {} {}\n".format(save_name, off_x0, off_y0, off_x1, off_y1)
+                        part_file.write(record)
+                        part_file.flush()
 
                     elif area < 0.3 and area_min < 0.3 and count_neg < COUNT:
                         count_neg += 1
                         dbg("negative", lv=0)
                         crop_img = img[neg_y0:neg_y1, neg_x0:neg_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(neg_dir, i, count_neg)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 0 0 0 0 0\n".format(save_name)
+                        neg_file.write(record)
+                        neg_file.flush()
 
                     # Positive / Part
                     p_side = min(height, width)
-                    off_cx = cx + round(random.uniform(- SCALE, SCALE) * p_side // 2)  # Todo
-                    off_cy = cy + round(random.uniform(- SCALE, SCALE) * p_side // 2)  # Todo
-                    p_side = random.randint(size, min(off_cx, off_cy, w - off_cx, h - off_cy))  # Todo
-                    off_x0 = off_cx - p_side
-                    off_y0 = off_cy - p_side
-                    off_x1 = off_x0 + p_side * 2
-                    off_y1 = off_y0 + p_side * 2
+                    pp_cx = cx + round(random.uniform(- SCALE, SCALE) * p_side // 2)  # Todo
+                    pp_cy = cy + round(random.uniform(- SCALE, SCALE) * p_side // 2)  # Todo
+                    pp_side = random.randint(size, min(pp_cx, pp_cy, w - pp_cx, h - pp_cy))  # Todo
+                    pp_x0 = pp_cx - pp_side
+                    pp_y0 = pp_cy - pp_side
+                    pp_x1 = pp_x0 + pp_side * 2
+                    pp_y1 = pp_y0 + pp_side * 2
 
-                    boxes = [off_x0, off_y0, off_x1, off_y1]
+                    boxes = [pp_x0, pp_y0, pp_x1, pp_y1]
                     area = iou(boxes, box, False)
                     area_min = iou(boxes, box, True)
                     dbg(area, area_min, lv=0)
+
+                    off_x0 = float((x0 - pp_x0)) / pp_side
+                    off_y0 = float((y0 - pp_y0)) / pp_side
+                    off_x1 = float((x1 - pp_x1)) / pp_side
+                    off_y1 = float((y1 - pp_y1)) / pp_side
+
                     if area > 0.65 and count_pos < COUNT:
                         count_pos += 1
                         dbg("positive", lv=0)
-                        crop_img = img[off_y0:off_y1, off_x0:off_x1]
+                        crop_img = img[pp_y0:pp_y1, pp_x0:pp_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(pos_dir, i, count_pos)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 1 {} {} {} {}\n".format(save_name, off_x0, off_y0, off_x1, off_y1)
+                        pos_file.write(record)
+                        pos_file.flush()
 
                     elif area > 0.4 and count_part < COUNT:
                         count_part += 1
                         dbg("part", lv=0)
-                        crop_img = img[off_y0:off_y1, off_x0:off_x1]
+                        crop_img = img[pp_y0:pp_y1, pp_x0:pp_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(part_dir, i, count_part)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 2 {} {} {} {}\n".format(save_name, off_x0, off_y0, off_x1, off_y1)
+                        part_file.write(record)
+                        part_file.flush()
 
                     elif area < 0.3 and area_min < 0.5 and count_neg < COUNT:
                         count_neg += 1
                         dbg("negative", lv=0)
-                        crop_img = img[off_y0:off_y1, off_x0:off_x1]
+                        crop_img = img[pp_y0:pp_y1, pp_x0:pp_x1]
+                        crop_img = cv2.resize(crop_img, (size, size), interpolation=cv2.INTER_CUBIC)
                         save_name = "{}/{}_{}.jpg".format(neg_dir, i, count_neg)
                         cv2.imwrite(save_name, crop_img)
+                        record = "{} 0 0 0 0 0\n".format(save_name)
+                        neg_file.write(record)
+                        neg_file.flush()
 
                     dbg(img_name, count_pos, count_part, count_neg, lv=0)
                     if count_pos >= COUNT and count_part >= COUNT and count_neg >= COUNT:
                         break
 
                     # cv2.rectangle(img, (x0, y0), (x1, y1), (0, 255, 0), 2)
-                    # cv2.rectangle(img, (off_x0, off_y0), (off_x1, off_y1), (255, 0, 0), 2)
+                    # cv2.rectangle(img, (pp_x0, pp_y0), (pp_x1, pp_y1), (255, 0, 0), 2)
                     # cv2.rectangle(img, (neg_x0, neg_y0), (neg_x1, neg_y1), (0, 0, 255), 2)
                     # cv2.imshow("image", img)
                     # cv2.waitKey(0)
